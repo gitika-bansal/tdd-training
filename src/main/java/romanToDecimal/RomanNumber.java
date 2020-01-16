eabc91c1806d958dcbce2f840415bffa7c2e0fa4
Gitika Bansal <gitika.bansal@springernature.com>
2020-01-16 14:07:05 +0530

    Added logic for complex roman numerals

--- src/main/java/romanToDecimal/RomanNumber.java
+++ src/main/java/romanToDecimal/RomanNumber.java
@@ -1,4 +1,36 @@
 package romanToDecimal;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class RomanNumber {
+
+    private String raw;
+
+
+    public RomanNumber(String raw) {
+        this.raw = raw;
+    }
+
+    public Integer convertToDecimal() {
+        Integer sum =0;
+        Integer previousValue = 0;
+        char[] digits = new StringBuilder(raw).reverse().toString().toCharArray();
+
+        for (char d:digits){
+           RomanDigits r = RomanDigits.valueOf(String.valueOf(d));
+           Integer decimalValue = r.getDecimalValue();
+           Integer valueToBeAdded = 0;
+           if(previousValue > decimalValue){
+               valueToBeAdded = decimalValue * -1;
+           }else{
+               valueToBeAdded = decimalValue;
+           }
+           sum = sum+valueToBeAdded;
+           previousValue = decimalValue;
+        }
+    return sum;
+    }
+
+
 }
--- src/test/java/romanToDecimal/RomanToDecimalTests.java
+++ src/test/java/romanToDecimal/RomanToDecimalTests.java
@@ -1,4 +1,42 @@
 package romanToDecimal;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class RomanToDecimalTests {
+
+
+    @Test
+    public void shouldConvertRomanDigitstoDecimal(){
+        List<RomanDigits> romanDigits = Arrays.asList(RomanDigits.values());
+        for(RomanDigits s: romanDigits){
+            RomanNumber number = new RomanNumber(s.toString());
+            assert(s.getDecimalValue().equals(number.convertToDecimal()));
+        }
+    }
+
+    @Test
+    public void shouldConvertRomanTwoToDecimal(){
+        RomanNumber number = new RomanNumber("II");
+        Integer decimal = number.convertToDecimal();
+        assert (new Integer(2).equals(decimal));
+    }
+    @Test
+    public void shouldConvertRomanThreeToDecimal(){
+        RomanNumber number = new RomanNumber("III");
+        Integer decimal = number.convertToDecimal();
+        assert (new Integer(3).equals(decimal));
+    }
+
+    @Test
+    public void shouldConvertRomanFouroDecimal(){
+        RomanNumber number = new RomanNumber("IV");
+        Integer decimal = number.convertToDecimal();
+        assert (new Integer(4).equals(decimal));
+    }
+
+
+
 }
